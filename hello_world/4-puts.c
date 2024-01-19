1e4f936814ba4f41edcdefa20b5bac493b33f4f1
BPChance <bradenpchance6@gmail.com>
2024-01-19 02:33:28 +0000

    4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(void)
 {
-	// prints a message using puts
+	/* prints a message using puts */
 const char *string = "\"Programming is like building a multilingual puzzle";
 puts(string);
 	return (0);
