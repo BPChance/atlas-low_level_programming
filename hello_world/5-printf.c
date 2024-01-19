94e031a75825411fcaad96e47a956d6f93cc2340
BPChance <bradenpchance6@gmail.com>
2024-01-19 03:14:12 +0000

    5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main - this program uses printf to output
+ *Return: 0
+ */
+int main()
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
