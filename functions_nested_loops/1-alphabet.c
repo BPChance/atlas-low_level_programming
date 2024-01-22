db72adefe20d8e4cf8c2a0e7e4e0a8747d8ef821
BPChance <bradenpchance6@gmail.com>
2024-01-22 05:40:55 +0000

    1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
- * main - check the code
- * Return: 0
- */
+ *  * main - check the code
+ *   * Return: 0
+ *    */
 void print_alphabet(void)
 {
-int c;
-for (c = 'a'; c <= 'z'; c++)
-{
-_putchar(c);
-}
-_putchar('\n');
+	int c;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
 }
