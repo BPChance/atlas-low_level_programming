c5269f46f016d7d7dd8796da64e42ca13b1f2277
BPChance <bradenpchance6@gmail.com>
2024-01-22 04:40:47 +0000

    0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* main - entry point
+* Return: 0
+*/
+int main(void)
+{
+	char a[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(a[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
