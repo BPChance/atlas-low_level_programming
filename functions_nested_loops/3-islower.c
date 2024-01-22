d5e19673a0bfecf18c7183f7f1f9e751759abb8e
BPChance <bradenpchance6@gmail.com>
2024-01-22 22:48:46 +0000

    3

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -3,9 +3,10 @@
  *_islower - checks for lowercase character
  * Return: 1 if true 0 if false 
  */
-int _islower(int c) {
-if (c >= 'a' && c <= 'z')
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
 	return (1);
-else
+	else
 	return (0);
 }
