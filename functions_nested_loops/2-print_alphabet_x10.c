e09fa04551e18b477888c117601b94f12c74c58d
BPChance <bradenpchance6@gmail.com>
2024-01-22 06:13:56 +0000

    2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints alphabet 10x
+ * Return: nothing
+ */
+void print_alphabet_x10(void)
+{
+int i = 0;
+while (i <= 10)
+{
+	char j;
+	for (j = 'a'; j<= 'z'; j++)
+	{
+		_putchar(j);
+	}
+	_putchar('\n');
+	i++;
+}
+}
