ee8dc93368428221564f0ed6505b9426e9c57907
BPChance <bradenpchance6@gmail.com>
2024-01-22 23:56:21 +0000

    6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer
+ * @x: integer output
+ * Return: Absolute value
+ */
+int _abs(int x)
+{
+	{
+		    if (x < 0) {
+			            return -x;
+				        } else {
+						        return x;
+							    }
+	}
+}
