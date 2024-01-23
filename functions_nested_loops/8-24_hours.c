625f94feb878e363b88a95fecba63eab5bc36298
BPChance <bradenpchance6@gmail.com>
2024-01-23 00:53:47 +0000

    8

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *
+ *
+ */
+void jack_bauer(void)
+{
+	char hour,minute;
+	    for (hour = '0'; hour <= '9'; hour++)
+	    {
+		            for (minute = '0'; minute <= '9'; minute++)
+			    {
+				                putchar(hour);
+						            putchar(hour);
+							                putchar(':');
+									            putchar(minute);
+										                putchar(minute);
+												            putchar('\n');													            }			        }
+}
