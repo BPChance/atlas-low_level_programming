93850d9f5cca33a44228bb74290d9a7ea00bb15d
BPChance <bradenpchance6@gmail.com>
2024-01-23 00:13:52 +0000

    7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_last_digit - prints last digit
+ * @q: integer
+ * Return: value of the last digit
+ */
+int print_last_digit(int q)
+{
+	int num = q % 10;
+	_putchar('0' + num);
+	return (num);
+}
