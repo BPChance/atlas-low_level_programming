67f3ab3a5b9ff42243c04b104c06736d71895bc8
BPChance <bradenpchance6@gmail.com>
2024-01-28 03:30:13 +0000

    5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * more_numbers - entry point
+ *
+ * Return: nothing
+ */
+void more_numbers(void)
+{
+	int c, i;
+	
+	for (i = 0; i < 10; i++)
+	{
+		for (c = 0; c <= 14; c++)
+		{
+		if (c >= 10)
+			_putchar('0' + c / 10);
+		_putchar('0' + c % 10);
+		}
+		_putchar('\n');
+	}
+}
