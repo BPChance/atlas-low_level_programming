6ed42598ec899b41e7c97b3cc7419691448e05f2
BPChance <bradenpchance6@gmail.com>
2024-01-28 10:07:56 +0000

    10

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -1,10 +1,28 @@
 #include "main.h"
 /**
+ * print_triangle - entry point
  *
- *
- *
+ * Return: nothing
  */
 void print_triangle(int size)
 {
+	int c, a;
 
+	if (size > 0)
+	{
+		for (c = 1; c <= size; c++)
+		{
+			for ((a = size - c); a > 0; a--)
+			_putchar(' ');
+
+			for (a = 0; a < c; a++)
+			_putchar('#');
+
+			if (c == size)
+			continue;
+
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
 }
