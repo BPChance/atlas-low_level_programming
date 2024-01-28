50b6f22f689b1040e59a2cf87bdb8b0a4e76a37e
BPChance <bradenpchance6@gmail.com>
2024-01-28 05:50:45 +0000

    9

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * main - entry point
+ * @a: integer
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 1; a < 101; a++)
+	{
+		if ((a % 3 == 0) && (a % 5 == 0))
+		{
+		printf("FizzBuzz ");
+		}
+		else if (a % 3 == 0)
+		{
+		printf("Fizz ");
+		}
+		else if (a % 5 == 0)
+		{
+		printf("Buzz ");
+		}
+		else 
+		{
+		printf("%d ",a);
+		}
+	}
+	return(0);
+}
