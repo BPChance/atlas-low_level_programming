953bc86dae011c168e20836fa60981a10a5a6e12
BPChance <bradenpchance6@gmail.com>
2024-01-28 04:35:38 +0000

    7

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -6,14 +6,14 @@
  */
 void print_diagonal(int n)
 {
-	int i,j;
+	int i, j;
 
-	if(n <= 0)
+	if (n <= 0)
 	{
 		_putchar('\n');
 		return;
 	}
-	for(i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 	for (j = 0; j < i; j++)
 	{
