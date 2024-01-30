4a0ecae37b6e9d36eacbc4bbe64d23e882ef9838
BPChance <bradenpchance6@gmail.com>
2024-01-30 04:47:51 +0000

    8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_array - entry point
+ * @a: integer
+ * @n: integer
+ * Return: nothing
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i < n - 1)
+			printf(" ,");
+	}
+	printf("\n");
+}
