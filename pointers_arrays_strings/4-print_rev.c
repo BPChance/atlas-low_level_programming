899cb3a99f08ec98dbeff4b0794702f5a75d9735
BPChance <bradenpchance6@gmail.com>
2024-01-30 01:56:56 +0000

    4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_rev - entry point
+ * @s: char
+ * Return: nothing
+ */
+void print_rev(char *s)
+{
+	int i, length = strlen(s);
+	
+	for (i = length - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
