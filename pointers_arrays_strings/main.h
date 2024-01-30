4011b67a80169373c48ae0601b6979b0429be40a
BPChance <bradenpchance6@gmail.com>
2024-01-30 04:28:21 +0000

    7

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -29,6 +29,7 @@ void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
+void puts_half(char *str);
 #include <ctype.h>
 #include <stdio.h>
 #include <string.h>
