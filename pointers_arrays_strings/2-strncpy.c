03da72659229438533b0f626c753899b9916aa34
BPChance <bradenpchance6@gmail.com>
2024-02-04 18:04:07 +0000

    strncpy

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,7 +1,9 @@
 #include "main.h"
 /**
  * *_strncpy - entry point
- *
+ * @dest: pointer to a char
+ * @src: pointer to a char
+ * @n: int
  * Return: dest
  */
 char *_strncpy(char *dest, char *src, int n)
