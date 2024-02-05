22b9a944a9f7ec3d71be5a171f5bd43f3a3beb15
BPChance <bradenpchance6@gmail.com>
2024-02-05 19:41:32 +0000

    memcpy

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -8,11 +8,11 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int i = 0;
+	unsigned int i = 0;
 
-	for (; n > 0; i++)
+	for (; i < n; i++)
 	{
-		dest[i - 1] = *src;
+		dest[i] = src[i];
 		n--;
 	}
 	return (dest);
