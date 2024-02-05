75d08dda0b63b820c9c8d9ca9ab5d8ee7f300cb2
BPChance <bradenpchance6@gmail.com>
2024-02-05 04:48:57 +0000

    cap

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ * cap_string - entry point
+ * @str: pointer to a char
+ * Return: str
+ */
+char *cap_string(char *str)
+{
+	int new_word = 1;
+	int i;
+
+	for (i = 0; str[i]; i++)
+	{
+		switch (str[i])
+		{
+			case ' ':
+			case '\t':
+			case '\n':
+			case ',':
+			case ';':
+			case '.':
+			case '!':
+			case '?':
+			case '"':
+			case '(':
+			case ')':
+			case '{':
+			case '}':
+			new_word = 1;
+			break;
+		default:
+			if (new_word && isalpha(str[i]))
+			{
+				str[i] = toupper((unsigned char)str[i]);
+				new_word = 0;
+			}
+			break;
+		}
+	}
+	return (str);
+}
