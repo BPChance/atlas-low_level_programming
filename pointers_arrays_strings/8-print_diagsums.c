5e373f7976010d3d72603f87982e0ca3da7c2765
BPChance <bradenpchance6@gmail.com>
2024-02-06 06:01:10 +0000

    diagsum

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -6,23 +6,14 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i, j;
+	int i;
 	int mainsum = 0;
 	int antisum = 0;
 
-	for (i = 0; i < size - 1; i++)
+	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < size - 1; j++)
-		{
-			if (i == j)
-			{
-			mainsum += a[i * size + i];
-			}
-			if (i + j == size - 1)
-			{
-			antisum += a[i * size + (size - 1 - i)];
-			}
-		}
+		mainsum += a[i * size + i];
+		antisum += a[i * size + (size - i - 1)];
 	}
 	printf("%d, %d\n", mainsum, antisum);
 }
