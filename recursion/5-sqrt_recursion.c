a16cacee7319b932c9f8635bba57c1151e227cb8
BPChance <bradenpchance6@gmail.com>
2024-02-11 18:37:47 +0000

    5

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * _sqrt_recursion - entry point
- * @n: int value
- * Return: natural square root of a number
+ * _sqrt - entry point
  * @x: int value
  * @y: int value
+ * Return: square root
  */
+
 int _sqrt(int x, int y)
 {
 	if (y * y == x)
@@ -15,9 +15,11 @@ int _sqrt(int x, int y)
 	return (_sqrt(x, y + 1));
 }
 /**
- * _sqrt - entry point
- * Return: square root of a number
+ * _sqrt_recursion - entry point
+ * @n: int value
+ * Return: natural square root of a number
  */
+
 int _sqrt_recursion(int n)
 {
 	return (_sqrt(n, 1));
