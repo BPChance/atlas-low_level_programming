4c26ec2d3f0549165274588c3f8d30533cd21c29
BPChance <bradenpchance6@gmail.com>
2024-02-11 23:47:51 +0000

    static

---
+++ static_libraries/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *  * _isupper - checks if character is uppercase
+ *   * @c: integer value c
+ *    * Return: 1 if true, else 0 if false
+ *     */
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+return (1);
+else
+return (0);
+}
