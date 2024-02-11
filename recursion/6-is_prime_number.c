f1d76fb4c05690531f9bcd2d101d4ccd39faf778
BPChance <bradenpchance6@gmail.com>
2024-02-11 22:07:15 +0000

    6

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,24 +1,38 @@
 #include "main.h"
 /**
- * _is_prime_number - entry point
+ * is_prime_number - entry point
  * @n: int value
  * Return: 1 if prime, otherwise 0
  */
+
+int prime(int n, int i);
+
 int is_prime_number(int n)
 {
-	int i;
-
 	if (n <= 1)
 	{
 		return (0);
 	}
-
-	for (i = 2; i * i <=n; i++)
+	return (prime(n, 2));
+}
+/**
+ * prime - entry point
+ * @n: int value
+ * @i: int value
+ * Return: prime
+ */
+int prime(int n, int i)
+{
+	if (i == n)
 	{
-		if (n % i == 0)
-		{
-			return (0);
-		}
+		return (1);
 	}
-	return (1);
+	else if (n % i == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (prime(n, i + 1));
+	}
 }
