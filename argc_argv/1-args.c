708b0d27d388b14a74701213922d7eada477f24d
BPChance <bradenpchance6@gmail.com>
2024-02-12 19:42:24 +0000

    1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	if (argv)
+	{
+		printf("%d\n", argc);
+	}
+	return (0);
+}
