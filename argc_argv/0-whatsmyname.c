a5cdb7e7e42c371c969c13f54d6b76a2b7bb4f13
BPChance <bradenpchance6@gmail.com>
2024-02-12 19:29:40 +0000

    0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - entry level
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0
+ */
+int main (int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+	printf("%s\n", argv[0]);
+	}
+	return (0);
+}
