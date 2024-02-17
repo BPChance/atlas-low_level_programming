5aad5eb94a504577e8f2d8be7b94d217ad951ab7
BPChance <bradenpchance6@gmail.com>
2024-02-17 20:35:29 +0000

    2

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *result;
+	int len_s1, len_s2;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+	len_s1 = strlen(s1);
+	len_s2 = strlen(s2);
+
+	result = (char*)malloc(len_s1 + len_s2 + 1);
+
+	if (result == NULL)
+	{
+		return (NULL);
+	}
+	strcpy(result, s1);
+	strcat(result, s2);
+
+	return (result);
+}
