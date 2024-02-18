4ec97757ac1d940e9b6b393cec1d3d2c6caf42f1
root <bradenpchance6@gmail.com>
2024-02-18 22:48:06 +0000

    2

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _calloc - entry point
+ * @nmemb: number of elements
+ * @size: size of elements in bytes
+ * Return: ptr
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	ptr = malloc(nmemb * size);
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < nmemb * size; i++)
+	{
+		((char *)ptr)[i] = 0;
+	}
+	return (ptr);
+}
