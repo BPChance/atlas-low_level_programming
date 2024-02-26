c476ade7c6fddcf2187e25856ac6d39dd3c69d42
root <bradenpchance6@gmail.com>
2024-02-26 10:19:56 +0000

    awesome calculator

---
+++ function_pointers/3-main.c
@@ -0,0 +1,37 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main function
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+	int (*func_ptr)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+	func_ptr = get_op_func(argv[2]);
+
+	if (func_ptr == NULL)
+	{
+		printf("Error\n");
+		return (99);
+	}
+
+	result = func_ptr(num1, num2);
+
+	printf("%d\n", result);
+
+	return (0);
+}
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,46 @@
+#include "3-calc.h"
+/**
+ * op_add - sum
+ * @a: int value
+ * @b: int value
+ */
+int op_add(int a, int b)
+{
+	return(a + b);
+}
+/**
+ * op_sub - difference
+ * @a: int
+ * @b: int
+ */
+int op_sub(int a, int b)
+{
+	return(a - b);
+}
+/**
+ * op_mul - product
+ * @a: int
+ * @b: int
+ */
+int op_mul(int a, int b)
+{
+	return(a * b);
+}
+/**
+ * op_div - result
+ * @a: int
+ * @b: int
+ */
+int op_div(int a, int b)
+{
+	return(a / b);
+}
+/**
+ * op_mod - remainder
+ * @a: int
+ * @b: int
+ */
+int op_mod(int a, int b)
+{
+	return(a % b);
+}
