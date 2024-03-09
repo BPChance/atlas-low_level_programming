5b1887426b450c6db4570e3472823a74ca9d23cd
root <bradenpchance6@gmail.com>
2024-03-09 20:21:07 +0000

    h->len, h->str

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -7,6 +7,7 @@
 size_t print_list(const list_t *h)
 {
 	size_t count = 0;
+
 	while (h != NULL)
 	{
 		if (h->str == NULL)
@@ -20,5 +21,5 @@ size_t print_list(const list_t *h)
 		h = h->next;
 		count++;
 	}
-	return(count);
+	return (count);
 }
