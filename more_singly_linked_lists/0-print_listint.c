517ec071d13140923c769f5999dd5b95b05886b1
root <bradenpchance6@gmail.com>
2024-03-11 04:04:25 +0000

    made it print the number of elements

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -1,8 +1,18 @@
 #include "lists.h"
 /**
- *
+ * print_listint - prints all elements
+ * @h: head
+ * Return: nodes
  */
 size_t print_listint(const listint_t *h)
 {
+	size_t nodes = 0;
 
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		nodes++;
+	}
+	return (nodes);
 }
